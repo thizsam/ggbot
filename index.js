b0c070433b108b87e83f5bd4a9e0a3039e466e37
thizsam <82769814+thizsam@users.noreply.github.com>
2021-06-20 16:36:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+﻿const { Client, Collection } = require("discord.js");
+const config = require("./config.json");
+const client = new Client();
+const { GiveawaysManager } = require('discord-giveaways');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Zero is sexy'));
+
+app.listen(port, () => console.log(`Bot Listening At http://localhost:${port}, Dm 0_0#6666 Your host link`));
+
+
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "💩"
+    }
+});
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
+
+["aliases", "commands"].forEach(x => client[x] = new Collection());
+["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
+
+
+client.login(config["Bot_Info"].token);
